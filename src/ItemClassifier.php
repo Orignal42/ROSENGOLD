971cfd0f75e9091f2e764b41b4875c3624940703
Orignal42 <rbarriquand@gmail.com>
2022-04-06 14:03:03 +0200

    5eme depot

--- src/ItemClassifier.php
+++ src/ItemClassifier.php
@@ -2,70 +2,38 @@
 
 namespace App;
 
-// use App\Updaters\SulfuraUpdater;
-// use App\Updaters\BackstagePassUpdater;
-// use App\Updaters\AgedBrieUpdater;
-// use App\Updaters\ConjuredItemCakeUpdater;
-// use App\ItemUpdater;
-
+use App\ItemUpdater;
 
 class ItemClassifier
 {
-    public function getUpdater():array
+
+    public function readDirectory()
     {
-    
+        $array = [];
         $updaters = scandir(__DIR__ . "/Updaters");
-        // lecture du repertoire
-        // On transforme le nom de fichier en nom de class_exists
-        $class = 'Class'.$updaters;
-        $object = new $class();
-        echo $object;
-        // On recupere le tableau des updates
-        return [];
-    }
-    public function categorize()
-    {
-        // $openUpdate = dir(__DIR__ . '/Updaters');
-        // foreach ($this->getUpdaters() as $u) {
-            // if ($updater::resolve($item))  {
-
-
-                // echo ("App/Updaters/".$updater . PHP_EOL);
-            // }
+        foreach ($updaters as $updater) {
+            if ($updater != ".." && $updater != ".") {
+                $updaterClass = "\\App\\Updaters\\" . str_replace(".php", "", $updater);
+                array_push($array, $updaterClass);
+            };
         }
+        return $array;
     }
 
-// }
-    // const BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = 'Backstage passes to a TAFKAL80ETC concert';
-    // const SULFURAS_HAND_OF_RAGNAROS = 'Sulfuras, Hand of Ragnaros';
-    // const AGED_BRIE = 'Aged Brie';
+    public function categorize($item)
+    {
+        $updater = $this->resolveUpdaters($item);
+        return new $updater;
+    }
 
-    // public function categorize($item)
-    // {
-    //     if ($item->name == self::SULFURAS_HAND_OF_RAGNAROS) {
-    //         $myItem = new SulfuraUpdater($item);
-    //     } else {
-    //         if ($item->name == self::BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT) {
-    //             $myItem = new BackstagePassUpdater($item);
-    //         } else {
-
-    //             if ($item->name == self::AGED_BRIE) {
-    //                 $myItem = new AgedBrieUpdater($item);
-    //             } else {
-    //                 if ($this->startsWith($item->name, 'Conjured')) {
-    //                     $myItem = new ConjuredItemCakeUpdater($item);
-    //                 } else {
-    //                     $myItem = new ItemUpdater($item);
-    //                 }
-    //             }
-    //         }
-    //     }
-    //     return $myItem;
-    // }
-
-
-    // function startsWith($string, $startString)
-    // {
-    //     $len = strlen($startString);
-    //     return (substr($string, 0, $len) === $startString);
-    // }
+    public function resolveUpdaters($item)
+    {
+        $files = $this->readDirectory();
+        foreach ($files as $u) {
+            if ($u::resolve($item)) {
+                return $u;
+            }
+        }
+        return new ItemUpdater($item);
+    }
+}
--- tests/DebugTest.php
+++ tests/DebugTest.php
@@ -2,16 +2,16 @@
 namespace Tests;
 
 use App\ItemClassifier;
+use App\Item;
 use PHPUnit\Framework\TestCase;
 
 
 class DebugTest  extends TestCase
 {
-    public function testClassifier():void
+    public function testClassifier(): void
     {
-
-        $itemclassifier= new ItemClassifier();
-        $itemclassifier->categorize();
+        $itemClassifier = new ItemClassifier();
+        $itemClassifier->resolveUpdaters(new Item("BrieTest", 10, 5));
     }
 
 
